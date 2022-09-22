d8b2cf47747b25e95d42390dad74702282f28be3
Michael Bardou <michael.bardou@icloud.com>
2022-09-22 16:54:13 +0200

    correct issue

--- app.vue
+++ app.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
-import { Gender, Popularity, Length, names } from "@/data";
+import { Gender, Length, Popularity, names } from "@/data";
 
 interface OptionsState {
   gender: Gender;
   popularity: Popularity;
   length: Length;
 }
-
 const options = reactive<OptionsState>({
   gender: Gender.GIRL,
   length: Length.SHORT,
   popularity: Popularity.TRENDY,
 });
-const selectedNames = ref<string[]>([]);
-
 const computeSelectedNames = () => {
   const filteredNames = names
     .filter((name) => name.gender === options.gender)
@@ -22,25 +19,24 @@ const computeSelectedNames = () => {
       if (options.length === Length.ALL) return true;
       else return name.length === options.length;
     });
-
   selectedNames.value = filteredNames.map((name) => name.name);
 };
-
+const selectedNames = ref<string[]>([]);
 const optionsArray = [
   {
     title: "1) Choose a gender",
-    categorie: "gender",
+    category: "gender",
     buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY],
   },
   {
     title: "2) Choose the name's popularity",
-    categorie: "popularity",
+    category: "popularity",
     buttons: [Popularity.TRENDY, Popularity.UNIQUE],
   },
   {
     title: "3) Choose name's length",
-    categorie: "length",
-    buttons: [Length.SHORT, Length.LONG, Length.ALL],
+    category: "length",
+    buttons: [Length.SHORT, Length.ALL, Length.LONG],
   },
 ];
 </script>
@@ -54,6 +50,7 @@ const optionsArray = [
         v-for="option in optionsArray"
         :key="option.title"
         :option="option"
+        :options="options"
       />
       <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
@@ -74,11 +71,18 @@ const optionsArray = [
   margin: 0 auto;
   text-align: center;
 }
-
 h1 {
   font-size: 3rem;
 }
-
+.options-container {
+  background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+}
 .primary {
   background-color: rgb(249, 87, 89);
   color: white;
@@ -89,13 +93,11 @@ h1 {
   margin-top: 1rem;
   cursor: pointer;
 }
-
 .cards-container {
   display: flex;
   margin-top: 3rem;
   flex-wrap: wrap;
 }
-
 .card {
   background-color: rgb(27, 60, 138);
   width: 28%;
@@ -106,53 +108,11 @@ h1 {
   margin-bottom: 1rem;
   position: relative;
 }
-
 .card p {
   position: absolute;
   top: -29%;
   left: 92.5%;
   cursor: pointer;
   color: rgba(255, 255, 255, 0.178);
-}
-</style>
-
-<style scoped>
-.options-container {
-  background-color: rgb(255, 238, 236);
-  border-radius: 2rem;
-  padding: 1rem;
-  width: 95%;
-  margin: 0 auto;
-  margin-top: 4rem;
-  position: relative;
-}
-
-.option-container {
-  margin-bottom: 2rem;
-}
-
-.option {
-  background: white;
-  outline: 0.15rem solid rgb(249, 87, 89);
-  border: none;
-  padding: 0.75rem;
-  width: 12rem;
-  font-size: 1rem;
-  color: rgb(27, 60, 138);
-  cursor: pointer;
-  font-weight: 200;
-}
-
-.option-left {
-  border-radius: 1rem 0 0 1rem;
-}
-
-.option-right {
-  border-radius: 0 1rem 1rem 0;
-}
-
-.option-active {
-  background-color: rgb(249, 87, 89);
-  color: white;
 }
 </style>
