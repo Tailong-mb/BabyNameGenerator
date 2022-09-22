a926e8add1dbff09a07f4d08a859a6f11c087c91
Michael Bardou <michael.bardou@icloud.com>
2022-09-22 16:06:25 +0200

    refactor

--- app.vue
+++ app.vue
@@ -24,6 +24,24 @@ const computeSelectedNames = () => {
 
   selectedNames.value = filteredNames.map((name) => name.name);
 };
+
+const optionsArray = [
+  {
+    title: "1) Choose a gender",
+    categorie: "gender",
+    buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY],
+  },
+  {
+    title: "2) Choose the name's popularity",
+    categorie: "popularity",
+    buttons: [Popularity.TRENDY, Popularity.UNIQUE],
+  },
+  {
+    title: "3) Choose name's length",
+    categorie: "length",
+    buttons: [Length.SHORT, Length.LONG, Length.ALL],
+  },
+];
 </script>
 
 <template>
@@ -31,77 +49,6 @@ const computeSelectedNames = () => {
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click the "Find Names" buttom below</p>
     <div class="options-container">
-      <div class="option-container">
-        <h4>1) Choose a gender</h4>
-        <div class="option-buttons">
-          <button
-            :class="options.gender === Gender.BOY && 'option-active'"
-            class="option option-left"
-            @click="options.gender = Gender.BOY"
-          >
-            Boy
-          </button>
-          <button
-            class="option"
-            :class="options.gender === Gender.UNISEX && 'option-active'"
-            @click="options.gender = Gender.UNISEX"
-          >
-            Unisex
-          </button>
-          <button
-            :class="options.gender === Gender.GIRL && 'option-active'"
-            class="option option-right"
-            @click="options.gender = Gender.GIRL"
-          >
-            Girl
-          </button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>2) Choose the name's popularity</h4>
-        <div class="option-buttons">
-          <button
-            class="option option-left"
-            :class="options.popularity === Popularity.TRENDY && 'option-active'"
-            @click="options.popularity = Popularity.TRENDY"
-          >
-            Trendy
-          </button>
-          <button
-            class="option option-right"
-            :class="options.popularity === Popularity.UNIQUE && 'option-active'"
-            @click="options.popularity = Popularity.UNIQUE"
-          >
-            Unique
-          </button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>2) Choose name's length</h4>
-        <div class="option-buttons">
-          <button
-            class="option option-left"
-            :class="options.length === Length.LONG && 'option-active'"
-            @click="options.length = Length.LONG"
-          >
-            Long
-          </button>
-          <button
-            class="option"
-            :class="options.length === Length.ALL && 'option-active'"
-            @click="options.length = Length.ALL"
-          >
-            All
-          </button>
-          <button
-            class="option option-right"
-            :class="options.length === Length.SHORT && 'option-active'"
-            @click="options.length = Length.SHORT"
-          >
-            Short
-          </button>
-        </div>
-      </div>
       <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
     <div class="cards-container">
---
+++ components/Option.vue
@@ -0,0 +1,25 @@
+<div class="option-container">
+    <h4>1) Choose a gender</h4>
+    <div class="option-buttons">
+      <button
+        :class="options.gender === Gender.BOY && 'option-active'"
+        class="option option-left"
+        @click="options.gender = Gender.BOY"
+      >
+        Boy
+      </button>
+      <button
+        class="option"
+        :class="options.gender === Gender.UNISEX && 'option-active'"
+        @click="options.gender = Gender.UNISEX"
+      >
+        Unisex
+      </button>
+      <button
+        :class="options.gender === Gender.GIRL && 'option-active'"
+        class="option option-right"
+        @click="options.gender = Gender.GIRL"
+      >
+        Girl
+      </button>
+    </div>
